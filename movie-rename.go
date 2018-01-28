44eb9977398b25162dbb49d28533525fd2a6ddee
Vincent Frentzel <vincent@frentzel.eu>
2018-01-28 20:03:20 +0100

    Refactoring

--- movie-rename.go
+++ movie-rename.go
@@ -8,27 +8,22 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+
+	"github.com/kennygrant/sanitize"
 	"github.com/ryanbradynd05/go-tmdb"
 )
 
-var moviePatterns = []string{`(?i)(\d{4})`}
+var moviePatterns = []string{`(.*?)[\W\s](\d{4})`}
 
 func strCleanupNonWord(str string) string {
 	regExp := regexp.MustCompile(`[\W_]+`)
 	return strings.Trim(regExp.ReplaceAllString(str, " "), ` 	`)
 }
 
-func strCleanupSymbols(str string) string {
-	regExp_space := regexp.MustCompile(`[/]`)
-	regExp_nspace := regexp.MustCompile(`[:]`)
-	return regExp_nspace.ReplaceAllString(
-		regExp_space.ReplaceAllString(str, " "),"")
-}
-
 func renameMovie(path string, movieName string, releaseYear string) (string, error) {
 	ext := filepath.Ext(path)
 	dir := filepath.Dir(path)
-	newName := strCleanupSymbols(movieName + " (" + releaseYear + ")" + ext)
+	newName := sanitize.Path(movieName + "." + releaseYear + ext)
 	newPath := filepath.Join(dir, newName)
 
 	if _, err := os.Stat(newPath); err == nil {
@@ -41,14 +36,10 @@ func renameMovie(path string, movieName string, releaseYear string) (string, err
 func MovieInfo(filename string) (string, string, error) {
 	for _, curPattern := range moviePatterns {
 		if matched, err := regexp.MatchString(curPattern, filename); err == nil && matched {
-			movieName := ""
-			releaseYear := ""
-			err = nil
-
 			regExp := regexp.MustCompile(curPattern)
-			movieName = strCleanupNonWord(regExp.Split(filename, -1)[0])
-			releaseYear = regExp.FindStringSubmatch(filename)[1]
-
+			splitMatch := regExp.FindStringSubmatch(filename)
+			movieName := strCleanupNonWord(splitMatch[1])
+			releaseYear := splitMatch[2]
 			return movieName, releaseYear, err
 		} else if err != nil {
 			return "", "", err
@@ -64,52 +55,42 @@ func main() {
 	for _, path := range flag.Args() {
 		fmt.Println("\nCurrent Name: ", filepath.Base(path))
 		movieName, releaseYear, err := MovieInfo(filepath.Base(path))
-
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-
 		fmt.Println("Movie: ", movieName, " Year: ", releaseYear)
-
 		moviesList, err := TMDb.SearchMovie(movieName, nil)
-
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-
 		if len(moviesList.Results) == 0 {
 			fmt.Println(errors.New("No match found."))
 			continue
 		}
-
 		var selectedMovie tmdb.MovieShort
-
 		if len(moviesList.Results) >= 1 && len(moviesList.Results[0].ReleaseDate) >= 4 &&
 			moviesList.Results[0].ReleaseDate[:4] == releaseYear {
-
 			selectedMovie = moviesList.Results[0]
-
 		} else {
-
 			for index, movie := range moviesList.Results {
 				fmt.Println("\nTMDB Match: ", index+1, " Movie: ", movie.Title, " Year: ", movie.ReleaseDate)
 			}
-
 			fmt.Println("\nSelect match:")
 			i := -1
 			fmt.Scanf("%d", &i)
+			if i-1 > len(moviesList.Results) {
+				fmt.Println("Invalid selection.")
+				continue
+			}
 			selectedMovie = moviesList.Results[i-1]
 		}
-
 		newName, err := renameMovie(path, selectedMovie.Title, selectedMovie.ReleaseDate[:4])
-
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-
 		fmt.Println("New Name: ", newName)
 	}
 }
